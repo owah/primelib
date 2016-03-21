e2c0e68cdf38052a8e7a3f11ff1111e79ca885cf
Etienne <owah@outlook.com>
2016-03-21 22:29:20 +0100

    Code layout

--- primelib/primelib.cpp
+++ primelib/primelib.cpp
@@ -12,63 +12,7 @@ bool primelib::check_prime_b(uint_fast64_t n_t)
     return true;
 }
 
-//void primelib::generatePrimeLUT(uint_fast64_t n, uint_fast64_t* &primeArray) // generate all primes below uint_fast64_t n
-//{
-//    uint_fast64_t primeArrayCounter = 0;
-//    if (n % 32 == 0)
-//    {
-//        primeArrayCounter = n / 32;
-//    }
-//    else
-//    {
-//        primeArrayCounter = n / 32 + 1;
-//    }
-//
-//    primeArray = new uint_fast64_t[primeArrayCounter];
-//
-//    primeArray[0] = 1'346'704'470;
-//
-//    for (uint_fast64_t i = 1; i < primeArrayCounter; i++)
-//    {
-//        primeArray[i] = 0;
-//
-//        for (uint_fast8_t j = 1; j < 32; j += 2)
-//        {
-//            if (primelib::check_prime_b(i * 32 + j))
-//            {
-//                primeArray[i] = primeArray[i] | 1 << (j - 1);
-//            }
-//        }
-//    }
-//}
-
-uint_fast64_t primelib::retrievePrime(uint_fast64_t n, uint_fast32_t * primeArray)
-{
-    uint_fast64_t lookupIndex = 0;
-    uint_fast8_t lookupSlot = 0;
-
-    if (n % 32 == 0)
-    {
-        lookupIndex = n / 32 - 1;
-    }
-    else
-    {
-        lookupIndex = n / 32;
-    }
-
-    lookupSlot = n - lookupIndex * 32;
-
-    if (primeArray[lookupIndex] & 1 << (lookupSlot - 1))
-    {
-        return n;
-    }
-    else
-    {
-        return uint_fast64_t(0);
-    }
-}
-
-void primelib::generatePrimeLUT(uint_fast64_t n, uint_fast32_t* &primeArray) // generate all primes below uint_fast64_t n
+void primelib::generatePrimeLUT(uint_fast64_t n, uint_fast32_t* &primeArray)
 {
     uint_fast64_t primeArrayCounter = 0;
     uint_fast64_t lookupIndex = 0;
@@ -105,16 +49,16 @@ void primelib::generatePrimeLUT(uint_fast64_t n, uint_fast32_t* &primeArray) // 
         }
 
         lookupSlot = i - lookupIndex * 32;
-        
+
         if (primeArray[lookupIndex] & 1 << (lookupSlot - 1))
         {
             uint_fast64_t j = 0;
             uint_fast64_t k = 0;
 
             while (i*i + k*i <= n)
-            {          
+            {
                 j = i*i + k*i;
-                if (j!= 0 && j % 32 == 0)
+                if (j != 0 && j % 32 == 0)
                 {
                     lookupIndex = j / 32 - 1;
                 }
@@ -126,9 +70,35 @@ void primelib::generatePrimeLUT(uint_fast64_t n, uint_fast32_t* &primeArray) // 
                 lookupSlot = j - lookupIndex * 32;
 
                 primeArray[lookupIndex] = primeArray[lookupIndex] & ~(1 << (lookupSlot - 1));
-                
+
                 k++;
-            } 
+            }
         }
+    }
+}
+
+uint_fast64_t primelib::retrievePrime(uint_fast64_t n, uint_fast32_t * primeArray)
+{
+    uint_fast64_t lookupIndex = 0;
+    uint_fast8_t lookupSlot = 0;
+
+    if (n % 32 == 0)
+    {
+        lookupIndex = n / 32 - 1;
+    }
+    else
+    {
+        lookupIndex = n / 32;
+    }
+
+    lookupSlot = n - lookupIndex * 32;
+
+    if (primeArray[lookupIndex] & 1 << (lookupSlot - 1))
+    {
+        return n;
+    }
+    else
+    {
+        return uint_fast64_t(0);
     }
 }
